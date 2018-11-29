195d8de0b15b37bf13206f437829cf900ffefb65
Angel Hernández <s14120039@alumnos.itsur.edu.mx>
2018-11-28 19:55:20 -0600

    Carga de Examenes
    Ahora se pueden cargar examenes a partir de una lista de examenes

---
+++ AndroidApp/tototlan/app/src/main/java/mx/edu/itsur/tototlan/tototlan/QuestionsContainer.java
@@ -0,0 +1,23 @@
+package mx.edu.itsur.tototlan.tototlan;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class QuestionsContainer extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_questions_container);
+
+    }
+
+
+}
