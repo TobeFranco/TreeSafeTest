2104bf389bfcc228a6bf8f846ed5644c737c6061
Angel Hernández <s14120039@alumnos.itsur.edu.mx>
2018-11-28 23:48:27 -0600

    MenuBar para cerrar sesión
    Se agregó un control que "cierra" sesión

--- AndroidApp/tototlan/app/src/main/java/mx/edu/itsur/tototlan/tototlan/Home.java
+++ AndroidApp/tototlan/app/src/main/java/mx/edu/itsur/tototlan/tototlan/Home.java
@@ -1,5 +1,6 @@
 package mx.edu.itsur.tototlan.tototlan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,7 +8,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity{
     BottomNavigationView bottomNavigationView;
@@ -15,6 +21,11 @@ public class Home extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.logout_bar);
+        setSupportActionBar(toolbar);
+
+
 
         bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
 
@@ -42,4 +53,23 @@ public class Home extends AppCompatActivity{
         transaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                FirebaseAuth.getInstance().signOut();
+                Intent mainActivity = new Intent(Home.this,MainActivity.class);
+                startActivity(mainActivity);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
